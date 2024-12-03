f9148add7d1ba45e17da0540bc7dfdc2761093a3
Andreas Johansson <ajoh@hms.se>
2024-12-03 11:14:43 +0100

    IDE projects generated with CMake will keep the original file tree structure.

--- abcc_driver.cmake
+++ abcc_driver.cmake
@@ -48,8 +48,107 @@ set(abcc_driver_SRCS
     ${ABCC_DRIVER_DIR}/src/host_objects/sync_object.c
 )
 
+# Complete list of source (.c) files inside the Anybus CompactCom Driver. 
+set(abcc_driver_INCS
+    ${ABCC_DRIVER_DIR}/src/abcc_command_sequencer.h
+    ${ABCC_DRIVER_DIR}/src/abcc_debug_error.h
+    ${ABCC_DRIVER_DIR}/src/abcc_driver_interface.h
+    ${ABCC_DRIVER_DIR}/src/abcc_handler.h
+    ${ABCC_DRIVER_DIR}/src/abcc_link.h
+    ${ABCC_DRIVER_DIR}/src/abcc_memory.h
+    ${ABCC_DRIVER_DIR}/src/abcc_segmentation.h
+    ${ABCC_DRIVER_DIR}/src/abcc_setup.h
+    ${ABCC_DRIVER_DIR}/src/abcc_timer.h
+    ${ABCC_DRIVER_DIR}/src/par/abcc_driver_parallel_interface.h
+    ${ABCC_DRIVER_DIR}/src/spi/abcc_crc32.h
+    ${ABCC_DRIVER_DIR}/src/spi/abcc_driver_spi_interface.h
+    ${ABCC_DRIVER_DIR}/src/serial/abcc_crc16.h
+    ${ABCC_DRIVER_DIR}/src/serial/abcc_driver_serial_interface.h
+    ${ABCC_DRIVER_DIR}/inc/abcc.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_application_data_interface.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_command_sequencer_interface.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_config.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_identification.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_object_config.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_port.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_system_adaptation.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_system_adaptation_parallel.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_system_adaptation_serial.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_system_adaptation_spi.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_types.h
+    ${ABCC_DRIVER_DIR}/inc/application_abcc_handler.h
+    ${ABCC_DRIVER_DIR}/inc/application_data_instance_config.h
+    ${ABCC_DRIVER_DIR}/inc/application_select_firmware.h
+    ${ABCC_DRIVER_DIR}/inc/host_objects/anybus_file_system_interface_object.h
+    ${ABCC_DRIVER_DIR}/inc/host_objects/application_data_object.h
+    ${ABCC_DRIVER_DIR}/inc/host_objects/application_object.h
+    ${ABCC_DRIVER_DIR}/inc/host_objects/assembly_mapping_object.h
+    ${ABCC_DRIVER_DIR}/inc/host_objects/ethernet_object.h
+    ${ABCC_DRIVER_DIR}/inc/host_objects/mqtt_object.h
+    ${ABCC_DRIVER_DIR}/inc/host_objects/opcua_object.h
+    ${ABCC_DRIVER_DIR}/inc/host_objects/safety_object.h
+    ${ABCC_DRIVER_DIR}/inc/host_objects/sync_object.h
+    ${ABCC_DRIVER_DIR}/inc/host_objects/network_objects/bacnet_object.h
+    ${ABCC_DRIVER_DIR}/inc/host_objects/network_objects/canopen_object.h
+    ${ABCC_DRIVER_DIR}/inc/host_objects/network_objects/cclink_ie_field_network_object.h
+    ${ABCC_DRIVER_DIR}/inc/host_objects/network_objects/cclink_ie_tsn_object.h
+    ${ABCC_DRIVER_DIR}/inc/host_objects/network_objects/cclink_object.h
+    ${ABCC_DRIVER_DIR}/inc/host_objects/network_objects/devicenet_object.h
+    ${ABCC_DRIVER_DIR}/inc/host_objects/network_objects/ethercat_object.h
+    ${ABCC_DRIVER_DIR}/inc/host_objects/network_objects/ethernet_powerlink_object.h
+    ${ABCC_DRIVER_DIR}/inc/host_objects/network_objects/ethernetip_object.h
+    ${ABCC_DRIVER_DIR}/inc/host_objects/network_objects/modbus_object.h
+    ${ABCC_DRIVER_DIR}/inc/host_objects/network_objects/profibus_dpv1_object.h
+    ${ABCC_DRIVER_DIR}/inc/host_objects/network_objects/profinet_io_object.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_add.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_asm.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_bac.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_ccl.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_cfn.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_ciet.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_cipid.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_cnt.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_cop.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_cpc.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_cpn.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_dev.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_dpv0di.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_dpv1.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_eco.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_ect.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_eip.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_eme.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_epl.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_er.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_etn.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_fsi.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_fusm.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_mdd.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_mod.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_mqtt.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_nwccl.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_nwcfn.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_nwdpv1.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_nwetn.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_nwpnio.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_opcua.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_pnam.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_pnio.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_safe.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_smtp.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_soc.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_src3.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_sync.h
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc/abp_time.h
+)
+
 # Creating a library target containing the Anybus CompactCom Driver.
-add_library(abcc_driver EXCLUDE_FROM_ALL ${abcc_driver_SRCS})
+add_library(abcc_driver EXCLUDE_FROM_ALL ${abcc_driver_SRCS} ${abcc_driver_INCS})
+
+# Keeping the file and directory tree structure of the Anybus CompactCom Driver when 
+# generating IDE projects.
+source_group(TREE ${ABCC_DRIVER_DIR} PREFIX "abcc_driver" FILES ${abcc_driver_SRCS} ${abcc_driver_INCS})
 
 # The user shall already have created the ABCC_DRIVER_INCLUDE_DIRS variable and added 
 # directories to user specific include (.h) files outside the Anybus CompactCom Driver
@@ -64,11 +163,11 @@ add_library(abcc_driver EXCLUDE_FROM_ALL ${abcc_driver_SRCS})
 # Directories inside the Anybus CompactCom Driver containing include (.h) files to be 
 # externally accessible is appended to the list ABCC_DRIVER_INCLUDE_DIRS.
 list(APPEND ABCC_DRIVER_INCLUDE_DIRS 
-    "${ABCC_DRIVER_DIR}/inc"
-    "${ABCC_DRIVER_DIR}/inc/abcc_abp/inc"
-    "${ABCC_DRIVER_DIR}/src"
-    "${ABCC_DRIVER_DIR}/inc/host_objects/network_objects"
-    "${ABCC_DRIVER_DIR}/inc/host_objects"
+    ${ABCC_DRIVER_DIR}/inc
+    ${ABCC_DRIVER_DIR}/inc/abcc_abp/inc
+    ${ABCC_DRIVER_DIR}/src
+    ${ABCC_DRIVER_DIR}/inc/host_objects/network_objects
+    ${ABCC_DRIVER_DIR}/inc/host_objects
 )
 
 # Adding all the Anybus CompactCom Driver related include (.h) files to the user host 
